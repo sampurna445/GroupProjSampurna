e9c00f7eb5df126fe3c385bb15a70ce65653ac8a
sampurna445 <128822770+sampurna445@users.noreply.github.com>
2023-11-21 09:35:25 +0000

    Implemented react hook form in Login screen

--- src/screens/LoginScreen/index.js
+++ src/screens/LoginScreen/index.js
@@ -1,93 +1,93 @@
 import React, {useState} from 'react';
 import {View, TextInput, Button, Alert} from 'react-native';
 import auth from '@react-native-firebase/auth';
+import {useForm} from 'react-hook-form';
+import {yupResolver} from '@hookform/resolvers/yup';
+import {InputControl} from '../../components';
+import {LogHelper} from '../../helpers';
+
+import * as yup from 'yup';
+
+const schema = yup.object().shape({
+  email: yup
+    .string()
+    .required('Email is required')
+    .email('Invalid email')
+    .matches(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/, 'Invalid email'),
+  password: yup
+    .string()
+    .required('Password is required')
+    .matches(
+      /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{6,}$/,
+      'password must contain only letters ,numbers and a special Char',
+    ),
+});
 
 const LoginScreen = ({navigation}) => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-
-  const isEmailValid = email => {
-    // A basic email validation check
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return emailRegex.test(email);
-  };
-
-  const isPasswordValid = password => {
-    // Password should be at least 6 characters, contain a number, and a special character
-    const passwordRegex =
-      /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{6,}$/;
-    return passwordRegex.test(password);
-  };
-
-  const handleLogin = async () => {
-    if (!email || !password) {
-      Alert.alert('Error', 'Please fill in all fields');
-      return;
-    }
-
-    if (!isEmailValid(email)) {
-      Alert.alert('Error', 'Please enter a valid email address');
-      return;
-    }
-
-    if (!isPasswordValid(password)) {
-      Alert.alert(
-        'Error',
-        'Password must be at least 6 characters and contain a number and a special character',
-      );
-      return;
-    }
-
-    try {
-      await auth().signInWithEmailAndPassword(email, password);
-      navigation.navigate('Dashboard');
-    } catch (error) {
-      Alert.alert('Error', error.message);
-    }
-  };
+  const {
+    control,
+    handleSubmit,
+    formState: {errors},
+  } = useForm({
+    mode: 'all',
+    resolver: yupResolver(schema),
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+  });
+  // const handleLogin = async () => {
+  //   try {
+  //     await auth().signInWithEmailAndPassword(email, password);
+  //     navigation.navigate('Dashboard');
+  //   } catch (error) {
+  //     Alert.alert('Error', error.message);
+  //   }
+  // };
 
   const handleRegister = () => {
-    // if (!email || !password) {
-    //   Alert.alert('Error', 'Please fill in all fields');
-    //   return;
-    // }
-
-    // if (!isEmailValid(email)) {
-    //   Alert.alert('Error', 'Please enter a valid email address');
-    //   return;
-    // }
-
-    // if (!isPasswordValid(password)) {
-    //   Alert.alert(
-    //     'Error',
-    //     'Password must be at least 6 characters and contain a number and a special character',
-    //   );
-    //   return;
-    // }
-
-    // try {
-    //   await auth().createUserWithEmailAndPassword(email, password);
-    //   navigation.navigate('Dashboard');
-    // } catch (error) {
-    //   Alert.alert('Error', error.message);
-    // }
     navigation.navigate('SignUp', {navigation});
   };
 
   return (
     <View>
-      <TextInput
-        placeholder="Email"
-        value={email}
-        onChangeText={text => setEmail(text)}
+      <InputControl
+        control={control}
+        name={'email'}
+        placeholder={'Enter Email'}
+        error={errors?.email}
       />
-      <TextInput
-        placeholder="Password"
-        value={password}
-        onChangeText={text => setPassword(text)}
-        secureTextEntry
+      <InputControl
+        control={control}
+        name={'password'}
+        placeholder={'Enter Password'}
+        error={errors?.password}
       />
-      <Button title="Login" onPress={handleLogin} />
+      <Button
+        title={'Login'}
+        onPress={handleSubmit(async formData => {
+          console.log(formData.password);
+          console.log(formData.email);
+
+          LogHelper.localServerLogging(
+            'form submit',
+            'form submit is clicked',
+            formData,
+          );
+
+          try {
+            await auth().signInWithEmailAndPassword(
+              formData.email,
+              formData.password,
+            );
+            navigation.navigate('Dashboard');
+          } catch (error) {
+            console.log('Error:  ', error.message);
+            Alert.alert('Error', error.message);
+          }
+          return;
+        })}
+      />
       <Button title="Register" onPress={handleRegister} />
     </View>
   );
